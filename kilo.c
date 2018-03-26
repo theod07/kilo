6de08cae1342d516e901bfdfde08459507c76abc
theod07 <theodore.do@gmail.com>
2018-03-26 00:34:51 -0700

    step 44: move the cursor

--- kilo.c
+++ kilo.c
@@ -155,6 +155,10 @@ void editorRefreshScreen() {
 
   editorDrawRows(&ab);
 
+  char buf[32];
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  abAppend(&ab, buf, strlen(buf));
+
   abAppend(&ab, "\x1b[H", 3);
   abAppend(&ab, "\x1b[?25h", 6);
 
