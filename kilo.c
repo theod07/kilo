6eff095cfe5bfe2f3183f0c1b0cc16b1b3213b97
theod07 <theodore.do@gmail.com>
2018-03-31 16:52:44 -0700

    step 76: limit scrolling to the right

--- kilo.c
+++ kilo.c
@@ -295,6 +295,8 @@ void editorRefreshScreen() {
 /*** input ***/
 
 void editorMoveCursor(int key) {
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
   switch (key) {
     case ARROW_LEFT:
       if (E.cx != 0) {
@@ -302,7 +304,9 @@ void editorMoveCursor(int key) {
       }
       break;
     case ARROW_RIGHT:
-      E.cx++;
+      if (row && E.cx < row->size) {
+        E.cx++;
+      }
       break;
     case ARROW_UP:
       if (E.cy != 0) {
